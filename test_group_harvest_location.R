b26f6aa7d8ad6dc3fe44418d45ae4d03316a8d33
peter-smallidge <pjs23@cornell.edu>
2020-10-16 12:02:18 -0400

    test script of plot data

---
+++ test_group_harvest_location.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+plot.data = read_csv("~/R/slash-wall-vegetation/plot-data-2019.csv")
+
+head(plot.data)
+
+site <- plot.data %>% 
+  filter(location =="interior" | location=="control") %>% 
+  select(harvest, location, ba) %>% 
+  group_by(harvest, location) %>% 
+  summarize(basal_area = mean(ba))
+
+site  
